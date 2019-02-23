d3840adf4ee1b79a919735e4a3163157a3bff159
Salterm27 <alterman27@hotmail.com>
2019-02-23 16:02:28 -0300

    Update MySis.sh

--- MySis.sh
+++ MySis.sh
@@ -2,6 +2,7 @@
 
 echo "installing python and packages"
 apt-get --assume-yes install python3.6
+sudo apt install python-pip
 pip install sciPy
 pip install numPy
 pip install Pandas
@@ -14,5 +15,5 @@ echo "installing sublime"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-get --assume-yes install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-apt-get --assume-yes updatre
+apt-get --assume-yes update
 apt-get --assume-yes install sublime-text

917371682797d624260e404bbce1a8915e6ddd4f
Salterm27 <alterman27@hotmail.com>
2019-02-23 15:15:08 -0300

    Update MySis.sh

--- MySis.sh
+++ MySis.sh
@@ -1,17 +1,16 @@
-APT = "sudo apt-get"
-PIP = "sudo pip install" 
+#!/bin/bash
+echo "updating system"
+apt-get update
+apt-get upgrade
+echo "installing python and packages"
+apt-get install python
+pip install sciPy
+pip install numPy
+pip install Pandas
+pip install openpyxl
+pip install matplotlib
+pip install pillow
+pip install requests
 
-
-
-#updates the current system
-echo "$($APT) update"
-sudo "$($APT) upgrade"
-
-echo "$($APT) install python"
-echo "$($PIP) sciPy"
-echo "$($PIP) numPy"
-echo "$($PIP) Pandas"
-echo "$($PIP) openpyxl"
-echo "$($PIP) matplotlib"
-echo "$($PIP) pillow"
-echo "$($PIP) requests"
+echo "installing sublime"
+apt-get install subl3

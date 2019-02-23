447a2dc1187e7365653627eb8134be4deb41b1b9
Salterm27 <alterman27@hotmail.com>
2019-02-23 15:17:41 -0300

    Update MySis.sh

--- MySis.sh
+++ MySis.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "updating system"
 apt-get update
-apt-get upgrade
+apt-get --assume-yes upgrade
+
 echo "installing python and packages"
-apt-get install python
+apt-get --assume-yes install python
 pip install sciPy
 pip install numPy
 pip install Pandas
@@ -13,4 +14,4 @@ pip install pillow
 pip install requests
 
 echo "installing sublime"
-apt-get install subl3
+apt-get --assume-yes install subl3
